7b60de726f91001e7291beeef7b420ac9dc0d258
Michika H <mmhweenii@gmail.com>
2023-04-25 14:18:06 +0200

    about page modified

--- kebab/core/forms.py
+++ kebab/core/forms.py
@@ -259,16 +259,18 @@ class CommentForm(forms.ModelForm):
     name = forms.CharField(
         max_length=80,
         widget=forms.TextInput(attrs={
-            'class': 'border-2 border-gray-400 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-amber-900 bg-amber-50',
+            'class': 'border-2 border-gray-400 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-amber-600 bg-slate-50',
             'placeholder': 'Your username'
         }))
     avg_rating = forms.ChoiceField(
         choices=SERVICE_CHOICES,
-        widget=forms.RadioSelect(attrs={'class': 'form-radio mt-4'
-                                        }))
+        widget=forms.Select(attrs={
+            'class': 'form-select mt-4 border-2 border-gray-400 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-amber-600 bg-slate-50',
+            'palceholder': '-- Rating --'                 
+        }))
     comment = forms.CharField(
         widget=forms.Textarea(attrs={
-            'class': 'border-2 border-gray-400 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-amber-900 bg-amber-50',
+            'class': 'border-2 border-gray-400 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-amber-600 bg-slate-50',
             'placeholder': 'Leave your comment here.'
         }))
 
